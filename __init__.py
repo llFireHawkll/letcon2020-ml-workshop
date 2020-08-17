8d1d3e301013b67a0b744e487edfba2efee66bd6
llFireHawkll <sparsh.dtt@gmail.com>
2020-08-17 14:14:27 +0530

    initializing package

---
+++ __init__.py
@@ -0,0 +1,11 @@
+'''
+File: __init__.py
+Project: letcon2020-ml-workshop
+File Created: Monday, 17th August 2020 1:18:01 pm
+Author: Sparsh Dutta (sparsh.dtt@gmail.com)
+-----
+Last Modified: Monday, 17th August 2020 1:18:02 pm
+Modified By: Sparsh Dutta (sparsh.dtt@gmail.com>)
+-----
+Copyright 2020 Sparsh Dutta
+'''
